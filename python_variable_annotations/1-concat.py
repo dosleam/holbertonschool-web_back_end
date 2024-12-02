ba3a24ec4e90df8cbcf814306719e0b242e0ec30
dosleam <9400@holbertonstudents.com>
2024-12-02 13:59:09 +0100

    fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""Module pour concaténer deux chaînes de caractères."""
+"""
+Module pour concaténer deux chaînes de caractères.
+"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """Retourne la concaténation de deux chaînes de caractères."""
-    
+    """
+    Retourne la concaténation de deux chaînes de caractères.
+    """
+
     return str1 + str2
