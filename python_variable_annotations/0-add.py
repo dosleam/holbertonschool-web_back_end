42eff8ef603f100dffb006e09aa96dafa60b4d7e
dosleam <9400@holbertonstudents.com>
2024-12-02 12:00:54 +0100

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+"""
+Module qui contient une fonction pour additionner deux nombres flottants.
+"""
+
 def add(a: float, b: float) -> float:
     """
-    Cette fonction prend deux nombres à virgule flottante en entrée et
-    retourne leur somme
+    Additionne deux nombres flottants.
+
+    Cette fonction prend deux nombres à virgule flottante en entrée et retourne
+    leur somme. Elle est utilisée pour effectuer des calculs simples de somme.
 
     Parameters:
-    a (float): Le premier nombre à ajouter
-    b (float): Le deuxième nombre à ajouter
+    a (float): Le premier nombre flottant à additionner.
+    b (float): Le deuxième nombre flottant à additionner.
 
     Returns:
-    float: La somme des deux nombres
+    float: La somme des deux nombres.
     """
     return a + b
