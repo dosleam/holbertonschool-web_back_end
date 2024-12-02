0bd571d8edfcd69f70e8b68c9341216fc2866951
dosleam <9400@holbertonstudents.com>
2024-12-02 11:21:43 +0100

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    Cette fonction prend deux nombres à virgule flottante en entrée et 
+    retourne leur somme.
+
+    Parameters:
+    a (float): Le premier nombre à ajouter.
+    b (float): Le deuxième nombre à ajouter.
+
+    Returns:
+    float: La somme des deux nombres.
+    
+    Exemple:
+    >>> add(1.11, 2.22)
+    3.33
+    """
     return a + b
